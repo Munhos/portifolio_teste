3dc16d6ee5516e630f9acd7a598f0429e45b5121
Julio Cesar <juliomtd23@gmail.com>
2023-07-09 17:00:49 -0300

    .

---
+++ projetos_react/1_modulo/fundamentos/src/App.jsx
@@ -0,0 +1,16 @@
+//COMPONENTES
+import FirstComponent from './components/FirstComponent'
+
+//CSS
+import './App.css'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <FirstComponent/>
+    </div>
+    )
+}
+
+export default App;
---
+++ projetos_react/1_modulo/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,12 @@
+//CSS
+import './styles/FirstComponent.css'
+
+const FirstComponent = () => {
+    return(
+        <div>
+            <h1>Primeira importação de componente</h1>
+        </div>
+    );
+};
+
+export default FirstComponent;
